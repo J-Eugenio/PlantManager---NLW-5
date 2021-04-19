8fd6141c366a7980bdfd52acfaefd7c3ee256d55
eug <eugeniopb4@gmail.com>
2021-04-19 15:41:51 -0300

    Initial Commit

---
+++ styles.ts
@@ -0,0 +1,17 @@
+import styled from 'styled-components/native';
+
+
+export const ButtonTest = styled.TouchableOpacity`
+  border: 1px solid black;
+  background-color: #c53030;
+  width: 200px;
+  height: 200px;
+  border-radius:10px;
+
+`;
+
+export const Container = styled.View`
+  flex: 1;
+  align-items: center;
+  justify-content: center;
+`;
