be7b7297aac9008c41f78e96f4699d3708f92a8c
Eug <eugeniopb4@gmail.com>
2021-04-19 22:24:28 -0300

    :sparkles: Spash screen, Pagina Inicial(Welcome)

--- App.tsx
+++ App.tsx
@@ -1,21 +1,9 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View, Button } from 'react-native';
+import { Welcome } from './src/pages/Welcome';
 import { ButtonTest, Container } from './styles';
 
 export default function App() {
-
-  function msg(){
-    console.log('TESTE MDG!');
-    
-  }
   return (
-    <Container >
-
-        <ButtonTest onPress={() => msg()} >
-          <Text>Button</Text>
-        </ButtonTest>
-
-    </Container>
+    <Welcome/>
   );
 }
