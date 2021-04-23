1c6c70641ca60d08d3607aa58de776058be2d87d
Eug <eugeniopb4@gmail.com>
2021-04-22 23:12:44 -0300

    Dia 3

---
+++ src/pages/PlantSelect.tsx
@@ -0,0 +1,200 @@
+import React, { useEffect, useState } from 'react';
+import {
+  View,
+  Text,
+  StyleSheet,
+  FlatList,
+  ActivityIndicator,
+} from 'react-native';
+import { EnviromentButton } from '../components/EnviromentButton';
+
+import { Header } from '../components/Header';
+import { Load } from '../components/Load';
+
+import colors from '../styles/colors';
+import fonts from '../styles/fonts';
+import api from '../services/api';
+import { PlantCardPrimary } from '../components/PlantCardPrimary';
+
+interface EnviromentProps {
+  key: string;
+  title: string;
+}
+
+interface PlantProps {
+  id: number;
+  name: string;
+  about: string;
+  water_tips: string;
+  photo: string;
+  environments: [string]
+  frequency: {
+    times:number;
+    repeat_every: string
+  }
+}
+
+export function PlantSelect() {
+  const [environments, setEnvironments] = useState<EnviromentProps[]>([]);
+  const [plants, setPlants] = useState<PlantProps[]>([]);
+  const [filteredPlants, setFilteredPlants] = useState<PlantProps[]>([]);
+  const [environmentsSelected, setEnvironmentsSelected] = useState('all');
+  const [loading, setLoading] = useState(true);
+
+  const [page, setPage] = useState(1);
+  const [loadingMore, setLoadingMore] = useState(false);
+  const [loadedAll, setLoadedAll] = useState(false);
+
+  // eslint-disable-next-line consistent-return
+  function handleEnvironmentSelected(environment: string) {
+    setEnvironmentsSelected(environment);
+
+    if (environment === 'all') { return setFilteredPlants(plants); }
+
+    const filtered = plants.filter((plant) => plant.environments.includes(environment));
+
+    setFilteredPlants(filtered);
+  }
+
+  // eslint-disable-next-line consistent-return
+  async function fetchPlants() {
+    const { data } = await api
+      .get(`plants?_sort=name&_order=asc&_page=${page}&_limit=8`);
+
+    if (!data) {
+      return setLoading(true);
+    }
+
+    if (page > 1) {
+      setPlants((oldValue) => [...oldValue, ...data]);
+      setFilteredPlants((oldValue) => [...oldValue, ...data]);
+    } else {
+      setPlants(data);
+      setFilteredPlants(data);
+    }
+
+    setLoading(false);
+    setLoadingMore(false);
+  }
+
+  function handleFetchMore(distance: number) {
+    if (distance < 1) {
+      return;
+    }
+
+    setLoadingMore(true);
+    setPage((oldValue) => oldValue + 1);
+    fetchPlants();
+  }
+
+  useEffect(() => {
+    async function fetchEnvironment() {
+      const { data } = await api
+        .get('plants_enviroments?_sort=title&order=asc');
+      setEnvironments([{
+        key: 'all',
+        title: 'Todos',
+      },
+      ...data]);
+    }
+
+    fetchEnvironment();
+  }, []);
+
+  useEffect(() => {
+    // eslint-disable-next-line consistent-return
+
+    fetchPlants();
+  }, []);
+
+  if (loading) {
+    return (
+      <Load />
+    );
+  }
+  return (
+    <View style={styles.container}>
+      <View style={styles.header}>
+        <Header />
+
+        <Text style={styles.title}>
+          Em qual ambiente
+        </Text>
+        <Text style={styles.subtitle}>
+          você quer colocar sua planta:
+        </Text>
+      </View>
+
+      <View>
+        <FlatList
+          data={environments}
+          renderItem={({ item }) => (
+            <EnviromentButton
+              title={item.title}
+              active={item.key === environmentsSelected}
+              onPress={() => handleEnvironmentSelected(item.key)}
+            />
+          )}
+          horizontal
+          showsHorizontalScrollIndicator={false}
+          contentContainerStyle={styles.enviromentList}
+        />
+      </View>
+
+      <View style={styles.plants}>
+        <FlatList
+          data={filteredPlants}
+          renderItem={({ item }) => (
+            <PlantCardPrimary data={item} />
+          )}
+          showsVerticalScrollIndicator={false}
+          numColumns={2}
+          onEndReachedThreshold={0.1}
+          onEndReached={({ distanceFromEnd }) => handleFetchMore(distanceFromEnd)}
+          ListFooterComponent={
+            loadingMore
+              ? <ActivityIndicator color={colors.green_dark} />
+              : <></>
+          }
+        />
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: colors.background,
+  },
+  header: {
+    paddingHorizontal: 30,
+  },
+  title: {
+    fontSize: 17,
+    color: colors.heading,
+    fontFamily: fonts.heading,
+    lineHeight: 20,
+    marginTop: 15,
+  },
+  subtitle: {
+    fontFamily: fonts.text,
+    fontSize: 17,
+    lineHeight: 20,
+    color: colors.heading,
+  },
+  enviromentList: {
+    height: 40,
+    justifyContent: 'center',
+    paddingBottom: 5,
+    marginLeft: 32,
+    marginVertical: 32,
+  },
+  plants: {
+    flex: 1,
+    paddingHorizontal: 32,
+    justifyContent: 'center',
+  },
+});
--- src/routes/stack.routes.tsx
+++ src/routes/stack.routes.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
 
-import { NavigationContainer } from '@react-navigation/native';
+import { NavigationContainer, useNavigation } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 
 import colors from '../styles/colors';
 import { Welcome } from '../pages/Welcome';
 import { UserIdentification } from '../pages/UserIdentification';
 import { Confirmation } from '../pages/Confirmation';
+import { PlantSelect } from '../pages/PlantSelect';
 
 const stackRoutes = createStackNavigator();
 
@@ -32,6 +33,11 @@ const AppRoutes: React.FC = () => (
     <stackRoutes.Screen
       name="Confirmation"
       component={Confirmation}
+    />
+
+    <stackRoutes.Screen
+      name="PlantSelect"
+      component={PlantSelect}
     />
 
   </stackRoutes.Navigator>
